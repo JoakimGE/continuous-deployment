1d7f4296d7e7794fcbac8baba33dd0cdd7b70107
Joakim E <joaedv97@gmail.com>
2025-11-11 19:56:46 +0100

    something

--- code/src/main.js
+++ code/src/main.js
@@ -3,6 +3,7 @@ import './style.css'
 
 const timeContainer = document.getElementById("time");
 const timeLeft = document.getElementById("seconds-left");
+let kfahjfa = 0;
 
 const render = () => {
   const now = new Date();
